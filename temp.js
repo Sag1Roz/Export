e8f2f87ae5d6f4a792d5c2d046bd689841ce1e97
Sag1Roz <sagi67070@gmail.com>
2023-10-06 10:29:48 +0300

    secend version

--- temp.js
+++ temp.js
@@ -1,20 +1,31 @@
+import { filterButton } from "./app (4).js";
+
 export function temp(data) {
   data.forEach((data) => {
     const div = document.createElement("div");
 
     div.innerHTML = `<div class="card">
         <div class="title">Title:${data.title}</div>
-        <div class="category">Category:${
-          data.category === undefined ? "default" : data.category
+        <div class="category">${
+          data.category === undefined ? "" : `Category:${data.category}`
         }</div>
-        <div class="price">Price:${
-          data.price === undefined ? "default" : data.price
+        <div class="price">${
+          data.price === undefined ? "" : `Price:${data.price}`
         }</div>
-            <div class="rating">Rating: ${
-              data.rating === undefined ? "default" : data.rating.rate
+            <div class="rating"> ${
+              data.rating === undefined ? "" : `Rating:${data.rating.rate}`
             }</div>
         </div>`;
 
     display.appendChild(div);
+  });
+}
+
+export function templateButton(buttons) {
+  buttons.forEach((button) => {
+    const buttonElement = document.createElement("button");
+    buttonElement.innerHTML = `${button.contact}`;
+    buttonDisplay.appendChild(buttonElement);
+    buttonElement.addEventListener("click", () => filterButton(button.value));
   });
 }
